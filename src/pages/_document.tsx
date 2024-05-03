6c2bb3143d30422541e6d34a3d5ac3b18c2500a5
TheScotsMav <thrill.seeking.maverick@gmail.com>
2024-05-03 17:30:37 +0100

    init

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,38 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  static nonce: string | string[] | undefined;
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    this.nonce = ctx.req?.headers["x-nonce"];
+    return await Document.getInitialProps(ctx);
+  }
+  render() {
+    const nonce =
+      MyDocument.nonce && typeof MyDocument.nonce === "string"
+        ? MyDocument.nonce
+        : undefined;
+    const csp = `object-src 'none'; base-uri 'none'; script-src 'nonce-${nonce}' ${process.env.NODE_ENV === 'development' ? `'unsafe-eval'` : ''};`;
+    return (
+      <Html lang="en">
+        <Head nonce={nonce}>
+          <meta httpEquiv="Content-Security-Policy" content={csp} />
+        </Head>
+        <body>
+          <Main />
+          <NextScript nonce={nonce} />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
